c3a3918286c503c641d34cc8c859982c1ced27a7
Andrew Lisin <andrey.lisin@gmail.com>
2012-11-10 18:17:44 +0700

    Cedete with simple configuration added.

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+;; cedet
+;;
+;; options below were taken from cedet INSTALL file
+;;
+(load-file "~/.emacs.d/lisp/cedet-1.1/common/cedet.el")
+(global-ede-mode 1)
+(semantic-load-enable-minimum-features)
+(semantic-load-enable-code-helpers)
+(semantic-load-enable-guady-code-helpers)
+(global-srecode-minor-mode 1)
+
 ;; adding custom plugins path to load-path
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
