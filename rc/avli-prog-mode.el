e6e44f16af244148af478aab0993cc20be05fa4f
Andrew Lisin <andrey.lisin@gmail.com>
2015-04-03 21:40:13 +0600

    Use F8 to toggle flycheck

--- rc/avli-prog-mode.el
+++ rc/avli-prog-mode.el
@@ -6,6 +6,8 @@
   (local-set-key "\C-c:" 'uncomment-region)
   (local-set-key "\C-c;" 'comment-region)
   (local-set-key "\C-c\C-c" 'comment-region)
+
+  (local-set-key (kbd "<f8>") 'flycheck-mode)
   )
 
 (add-hook 'prog-mode-hook 'avli/prog-mode-hook)
