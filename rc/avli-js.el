c47fd6981b7598b896205e0a4e0156eb3b0c58ee
Andrew Lisin <andrey.lisin@gmail.com>
2015-05-18 18:17:25 +0600

    Run nodejs-repl with C-c C-p

--- rc/avli-js.el
+++ rc/avli-js.el
@@ -1,5 +1,6 @@
 (add-to-list 'company-backends 'company-tern)
 (add-hook 'js3-mode-hook 'avli/prog-mode-hook)
 (add-hook 'js3-mode-hook 'tern-mode)
+(add-hook 'js3-mode-hook (lambda () (local-set-key (kbd "C-c C-p") 'nodejs-repl)))
 
 (provide 'avli-js)
