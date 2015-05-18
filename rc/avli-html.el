bd33b055eab7bfec8d0f82aa9fc8267dfe8ce78d
Andrew Lisin <andrey.lisin@gmail.com>
2015-05-18 13:15:57 +0600

    Open CSS files in web-mode

--- rc/avli-html.el
+++ rc/avli-html.el
@@ -6,6 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
 
 (add-hook 'web-mode-hook 'emmet-mode)
 
