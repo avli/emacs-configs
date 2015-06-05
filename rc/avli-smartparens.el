7672132559abc7e90075010ac2c6f866dbfff261
Andrew Lisin <andrey.lisin@gmail.com>
2015-06-05 16:35:57 +0600

    Better handling of curly braces

--- rc/avli-smartparens.el
+++ rc/avli-smartparens.el
@@ -1,3 +1,18 @@
 (require 'smartparens-config)
 
+;; For details see the discussion here:
+;; https://github.com/Fuco1/smartparens/issues/80
+
+(defun avli/create-newline-and-enter-sexp (&rest _ignored)
+  "Open a new brace or bracket expression, with relevant newlines and indent. "
+  (newline)
+  (indent-according-to-mode)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(sp-local-pair 'c++-mode "{" nil :post-handlers '((avli/create-newline-and-enter-sexp "RET")))
+(sp-local-pair 'c-mode "{" nil :post-handlers '((avli/create-newline-and-enter-sexp "RET")))
+(sp-local-pair 'java-mode "{" nil :post-handlers '((avli/create-newline-and-enter-sexp "RET")))
+;(sp-local-pair 'js3-mode "{" nil :post-handlers '((avli/create-newline-and-enter-sexp "RET")))
+
 (provide 'avli-smartparens)
