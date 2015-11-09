843ddfe6af1c9bca4784140f18d43bc4cacf9d40
Andrey Lisin <andrey.lisin@gmail.com>
2015-11-09 15:56:28 +0600

    Don't run flycheck automatically in console

--- rc/avli-prog-mode.el
+++ rc/avli-prog-mode.el
@@ -1,5 +1,10 @@
+(defun flycheck-setup ()
+  (when (not (display-graphic-p))
+    (setq flycheck-check-syntax-automatically nil)))
+
 (defun avli/prog-mode-hook ()
   (flycheck-mode 1)
+  (flycheck-setup)
   (smartparens-mode 1)
 
   (local-set-key "\C-c:" 'uncomment-region)
