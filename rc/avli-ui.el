1c117d65bc34b8456316f5f2dd95c35ee32f4fb2
Andrew Lisin <andrey.lisin@gmail.com>
2015-12-08 19:54:18 +0600

    More UI tweeks

--- rc/avli-ui.el
+++ rc/avli-ui.el
@@ -1,7 +1,18 @@
 (load-theme 'cyberpunk 't)
 
-(setq default-frame-alist '((font . "Menlo-12")))
-(setq-default cursor-type 'bar)
+
+(cond
+ ((string-equal system-type "darwin")
+  (setq default-frame-alist '((font . "Menlo-12"))))
+ ((string-equal system-type "gnu/linux")
+  ((setq default-frame-alist '((font . "DejaVu Sans Mono-12"))))))
+
+(setq-default cursor-type 'box)
+(set-face-attribute 'cursor nil :background "#96CBFE")
+(blink-cursor-mode 0)
+
+(global-hl-line-mode 1)
+(set-face-attribute 'hl-line nil :background "Grey9")
 
 (set-face-attribute 'mode-line nil :box nil)
 (set-face-attribute 'mode-line-inactive nil :box nil)
