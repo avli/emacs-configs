3068677b897de5d8c3f04f2dec62f07d31a96c6e
Andrey Lisin <andrey.lisin@gmail.com>
2015-12-28 00:07:13 +0600

    Old good tomorrow theme

--- rc/avli-ui.el
+++ rc/avli-ui.el
@@ -1,11 +1,16 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/third-party/blackboard-theme")
-(load-theme 'zenburn 't)
+(load-theme 'sanityinc-tomorrow-bright 't)
 
 (cond
  ((string-equal system-type "darwin")
-  (setq default-frame-alist '((font . "Menlo-12"))))
+  (setq default-frame-alist '((font . "Monaco-13"))))
  ((string-equal system-type "gnu/linux")
   ((setq default-frame-alist '((font . "DejaVu Sans Mono-12"))))))
+
+(set-face-attribute 'mode-line nil :box nil)
+
+(require 'term)
+(set-face-attribute 'term-color-black nil :background "#000000")
 
 (blink-cursor-mode 0)
 
