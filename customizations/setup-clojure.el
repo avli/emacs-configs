a2eff0fcad4412600e6ab43dd97d7f6522690fd1
Andrey Lisin <andrey.lisin@gmail.com>
2016-01-03 23:47:50 +0600

    Add settings for ClojureScript

--- customizations/setup-clojure.el
+++ customizations/setup-clojure.el
@@ -2,17 +2,21 @@
 ;; Clojure
 ;;;;
 
-;; Enable paredit for Clojure
+;; Enable paredit for Clojure and ClojureScript
 (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+(add-hook 'clojurescript-mode-hook 'enable-paredit-mode)
 
 ;; Rainbow parentheses
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'clojurescript-mode-hook 'rainbow-delimiters-mode)
 
 ;; Show function arguments hints in echo area
 (add-hook 'clojure-mode-hook 'eldoc-mode)
+(add-hook 'clojurescript-mode-hook 'eldoc-mode)
 
 ;; Turn on autocompletion. Company co-operates nicely with cider
 (add-hook 'clojure-mode-hook 'company-mode)
+(add-hook 'clojurescript-mode-hook 'company-mode)
 
 ;;;;
 ;; Clojure REPL
