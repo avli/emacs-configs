a74045ef6316ebe594795a82e4d9af13f552f2fc
Andrey Lisin <andrey.lisin@gmail.com>
2016-01-13 17:48:21 +0600

    Add reverse input Russian keyboard layout

--- customizations/editing.el
+++ customizations/editing.el
@@ -55,3 +55,34 @@
 ;; Call magit status with C-x g when editing file under version
 ;; control
 (global-set-key (kbd "C-x g") 'magit-status)
+
+;; This scary function was borrowed from
+;; http://ru-emacs.livejournal.com/82428.html and makes Emacs to
+;; understand commands when system keyboard layout is set on something
+;; different than English one
+(defun reverse-input-method (input-method)
+  "Build the reverse mapping of single letters from INPUT-METHOD."
+  (interactive
+   (list (read-input-method-name "Use input method (default current): ")))
+  (if (and input-method (symbolp input-method))
+      (setq input-method (symbol-name input-method)))
+  (let ((current current-input-method)
+        (modifiers '(nil (control) (meta) (control meta))))
+    (when input-method
+      (activate-input-method input-method))
+    (when (and current-input-method quail-keyboard-layout)
+      (dolist (map (cdr (quail-map)))
+        (let* ((to (car map))
+               (from (quail-get-translation
+                      (cadr map) (char-to-string to) 1)))
+          (when (and (characterp from) (characterp to))
+            (dolist (mod modifiers)
+              (define-key local-function-key-map
+                (vector (append mod (list from)))
+                (vector (append mod (list to)))))))))
+    (when input-method
+      (activate-input-method current))))
+
+;; If some other layout is used replace russian-computer with
+;; preferable input method
+(reverse-input-method 'russian-computer)
