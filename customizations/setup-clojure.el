74d6798ddcb4ad934cfa09dd9bbe76df991131e5
Andrey Lisin <andrey.lisin@gmail.com>
2016-03-24 15:42:19 +0600

    Remove clojurescript-mode hooks
    clojurescript-mode is derived from clojure-mode, so adding hooks twice
    is not needed

--- customizations/setup-clojure.el
+++ customizations/setup-clojure.el
@@ -2,21 +2,19 @@
 ;; Clojure
 ;;;;
 
-;; Enable paredit for Clojure and ClojureScript
-(add-hook 'clojure-mode-hook 'enable-paredit-mode)
-(add-hook 'clojurescript-mode-hook 'enable-paredit-mode)
+;; Enable paredit for Clojure and ClojureScript As clojurescript-mode
+;; is derived from the clojure-mode it's enough to add things to
+;; clojure-mode-hook
+(add-hook 'clojure-mode-hook 'paredit-mode)
 
 ;; Rainbow parentheses
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'clojurescript-mode-hook 'rainbow-delimiters-mode)
 
 ;; Show function arguments hints in echo area
 (add-hook 'clojure-mode-hook 'eldoc-mode)
-(add-hook 'clojurescript-mode-hook 'eldoc-mode)
 
 ;; Turn on autocompletion. Company co-operates nicely with cider
 (add-hook 'clojure-mode-hook 'company-mode)
-(add-hook 'clojurescript-mode-hook 'company-mode)
 
 ;;;;
 ;; Clojure REPL
@@ -26,4 +24,4 @@
 (add-hook 'cider-repl-mode-hook 'company-mode)
 
 ;; Turn on paredit in Clojure REPL
-(add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
