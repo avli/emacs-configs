dbf0ac4661c80554ea1b66bd92c8005fed1dfd51
Andrey Lisin <andrey.lisin@gmail.com>
2016-03-26 19:26:06 +0600

    Run Python interpreter when entering Python mode

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -15,3 +15,8 @@
 
 ;; Show function arguments hints in echo area
 (add-hook 'python-mode-hook 'eldoc-mode)
+
+;; Run Python interpreter when entering Python mode, without it eldoc
+;; will complain about Python is not running
+(add-hook 'python-mode-hook
+	  (lambda () (run-python (python-shell-parse-command))))
