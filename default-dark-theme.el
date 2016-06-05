8a3571525b5113f60379f8970ca68a474ef775bb
Andrey Lisin <andrey.lisin@gmail.com>
2016-06-05 20:42:59 +0600

    Simplify theme

--- default-dark-theme.el
+++ default-dark-theme.el
@@ -5,14 +5,6 @@
  'default-dark
  '(show-paren-match ((t (:background "#333399"))))
  '(region ((t (:background "#464740"))))
- '(default ((t (:inherit nil :stipple nil :background "#0C1021" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco"))))
- '(mode-line ((t (:background "#434343" :foreground "white" :height 0.9 :box nil))))
- '(mode-line-inactive ((t (:background "#222222" :foreground "grey42" :height 0.9 :box nil))))
- '(whitespace-newline ((t (:foreground "#393D4E" :weight normal))))
- '(whitespace-space ((t (:foreground "#393D4E"))))
- '(font-lock-string-face ((t (:foreground "#5BC039"))))
- '(font-lock-comment-face ((t (:foreground "#ACACAC"))))
- '(font-lock-function-name-face ((t (:foreground "#EE5E08"))))
- '(font-lock-keyword-face ((t (:foreground "#F3D82C")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Menlo")))))
 
 (provide-theme 'default-dark)
