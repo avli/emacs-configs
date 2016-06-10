f5b4c97b84ea81bea37ebfd5479f3fb067b291f8
Andrey Lisin <andrey.lisin@gmail.com>
2016-06-10 14:17:01 +0600

    Cleanup theme

--- default-dark-theme.el
+++ default-dark-theme.el
@@ -5,11 +5,10 @@
  'default-dark
  '(show-paren-match ((t (:background "#333399"))))
  '(region ((t (:background "#464740"))))
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Menlo"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco"))))
  '(company-tooltip ((t (:background "#cccccc" :foreground "black"))))
  '(company-scrollbar-bg ((t (:background "#999999"))))
  '(company-scrollbar-fg ((t (:background "#555555"))))
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco"))))
  '(mode-line ((t (:background "darkblue" :foreground "grey85" :box (:line-width -1 :style released-button))))))
 
 (provide-theme 'default-dark)
