3044f68fedfbe586f4c73fd0792c6bf9b303d4e4
Andrey Lisin <andrey.lisin@gmail.com>
2019-11-28 00:05:43 +0300

    Don't use `xterm-color-unfontify-region`
    There is no longer need to to set `font-lock-unfontify-region-function`
    to `xterm-color-unfontify-region` as the latter has been removed.

--- customizations/ui.el
+++ customizations/ui.el
@@ -68,8 +68,7 @@
 ;; setenv TERM xterm-256color
 (require 'xterm-color)
 (progn (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
-       (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
-       (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region))
+       (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions)))
 
 ;; Function for toggling dark and light theme
 (defun toggle-dark-light-theme ()
